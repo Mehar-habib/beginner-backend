785c57952fe3117aad69e76c2987fc3f99b26fe3
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-05-05 17:25:05 +0500

    add video file plugin, bcrypt the password in user file

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -46,5 +48,12 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+// pre -> bcrypt password just before save the data
+userSchema.pre("save", async function (next) {
+  // when call any field this plugin call, solution: if 'isModified' then bcrypt the password otherwise no
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
 
 export const User = mongoose.model("User", userSchema);
