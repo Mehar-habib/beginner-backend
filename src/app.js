24b6d43307dcbc4ed6fe715b2e29b4c785c65390
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-05-05 14:23:32 +0500

    config app.js and index.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // url data config
+app.use(express.static("public"));
+app.use(cookieParser()); // set and access cookie from user
+
+export { app };
