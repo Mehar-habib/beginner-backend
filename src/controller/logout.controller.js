04787d684f2837afbc611b303b899fbeb368bfd8
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-05-13 22:51:24 +0500

    logout, auth middleware and routes

--- src/controller/logout.controller.js
+++ src/controller/logout.controller.js
@@ -6,6 +6,26 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const logoutUser = asyncHandler(async (req, res) => {
   // remove cookies
   // remove refresh Token
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true, // jo return ma response new updated value milay gy
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"));
 });
 
 export { logoutUser };
