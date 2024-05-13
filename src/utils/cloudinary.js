1c068a76c2bb129edb02745fa5bf0cbedb482eaa
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-05-13 16:46:09 +0500

    cloudinary file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,16 +7,19 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadCloudinary = async (localFilePath) => {
+const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
     // upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     return null;
   }
 };
+
+export { uploadOnCloudinary };
